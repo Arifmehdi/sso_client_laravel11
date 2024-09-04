0c04ebd8acda891211093b7c4473130e30df62d6
Arifmehdi <mehediarif.du@gmail.com>
2024-09-04 12:45:19 +0600

    init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,2 @@
+<h1>SSO Login 2.0 Client</h1>
+<a href="/login">Login with OAuth 2.0</a>
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/login', function(Request $request){
+    $request->session()->put('state', $state =  Str::random(40));
+
+    $query = http_build_query([
+        // "client_id" => "9ced2b6a-50e8-4fcb-8524-c692199bfa4a",
+        "client_id" => "9ced50fc-723c-440c-8577-bd00750dece5",
+        "redirect_uri" => "http://localhost:8080/callback",
+        "response_type" => "code",
+        "scope" => "",
+        "state" => $state,
+    ]);
+
+    // return redirect()
+    return redirect("http://localhost:8000/oauth/authorize?".$query);
+});
+
+Route::get('/callback', function(Request $request){
+    $state = $request->session()->pull("state");
+
+    throw_unless(strlen($state) > 0 && $state = $request->state,
+    InvalidArgumentException::class);
+
+    $response = Http::asForm()->post(
+        "http://localhost:8000/oauth/token",
+        [
+            "grant_type" => "authorization_code",
+            "client_id" => "9ced50fc-723c-440c-8577-bd00750dece5",
+            "client_secret" => "oeWe35Gw1ZzM0SPygSJ4ixjba4GSwNvZemCfzrKf",
+            "redirect_uri" => "http://localhost:8080/callback",
+            "code" => $request->code,
+        ]
+    );
+
+    return $response->json();
+});
